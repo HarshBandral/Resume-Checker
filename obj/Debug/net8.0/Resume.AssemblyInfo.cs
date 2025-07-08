9eb486f4fc311122b7513db811d1014daafd82e3
qdevloper <qdeveloper.github@gmail.com>
2025-07-08 17:49:30 +0530

    first

--- obj/Debug/net8.0/Resume.AssemblyInfo.cs
+++ obj/Debug/net8.0/Resume.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Resume")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2df35d5f5ca81bdf75069e69de7019f7549bde51")]
 [assembly: System.Reflection.AssemblyProductAttribute("Resume")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Resume")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
